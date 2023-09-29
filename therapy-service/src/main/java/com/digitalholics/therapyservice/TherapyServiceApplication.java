5a476221000237ef28c8b5179d58b768fd94980c
Nicole Price <desarrollo.md@atpsites.com>
2023-09-28 22:28:49 -0500

    Initial commit

---
+++ therapy-service/src/main/java/com/digitalholics/therapyservice/TherapyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.theraphyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheraphyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheraphyServiceApplication.class, args);
+	}
+
+}
---
+++ therapy-service/src/test/java/com/example/theraphyservice/TheraphyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.theraphyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheraphyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
