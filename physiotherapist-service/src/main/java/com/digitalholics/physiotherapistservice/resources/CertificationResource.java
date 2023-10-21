c558b46ea620964b78a2824609306b2307fb57aa
Edgar Alexander Malca Rojas <83733898+EdGaR-MaLcA@users.noreply.github.com>
2023-10-21 08:14:33 -0500

    feat: added atributtes to resource

--- physiotherapist-service/src/main/java/com/digitalholics/physiotherapistservice/resources/CertificationResource.java
+++ physiotherapist-service/src/main/java/com/digitalholics/physiotherapistservice/resources/CertificationResource.java
@@ -10,8 +10,9 @@ import lombok.*;
 public class CertificationResource {
     private Integer id;
     private Integer physiotherapistId;
-    private String title;
     private String school;
     private Integer year;
     private String photoUrl;
+    private String title;
+    
 }
