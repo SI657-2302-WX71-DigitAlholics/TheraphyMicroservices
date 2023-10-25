ad92664319c1fbfe4cefdc29a386b8c0fee7c6bb
Nicole Price <u201921559@upc.edu.pe>
2023-10-25 08:09:22 -0500

    feat: Modified application.yml configuration in health-records service

--- HealthRecordAndExpertise-service/src/main/java/com/digitalholics/healthrecordandexpertiseservice/HealthRecordAndExpertiseServiceApplication.java
+++ HealthRecordAndExpertise-service/src/main/java/com/digitalholics/healthrecordandexpertiseservice/HealthRecordAndExpertiseServiceApplication.java
@@ -2,8 +2,9 @@ package com.digitalholics.healthrecordandexpertiseservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class HealthRecordAndExpertiseServiceApplication {
 
     public static void main(String[] args) {
